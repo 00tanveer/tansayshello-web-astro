de6b9e5b00b6b3e36a1575791d97f7e8631f6c54
00tanveer <tanveer_fh09@yahoo.com>
2023-10-08 22:36:47 +0600

    installed @fontsource/lekton

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'TanSaysHello - I love creating businesses, writing code and making friends.';
+export const SITE_DESCRIPTION = "I love creating businesses, writing code and making friends.";
